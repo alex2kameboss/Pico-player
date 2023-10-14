ef0e436d4997326c0c2d2d4d5136d155f0639457
Alexandru Puscasu <palexandru2000@gmail.com>
2023-10-14 13:08:26 +0300

    add two leds

--- main.c
+++ main.c
@@ -1,35 +1,39 @@
 #include "pico/stdlib.h"
-
 #include "FreeRTOS.h"
-
 #include "task.h"
 
-void vBlinkTask() {
+#define LED0 28
+#define LED1 27
 
+void vBlinkTask0() {
    for (;;) {
-
-      gpio_put(PICO_DEFAULT_LED_PIN, 1);
-
+      gpio_put(LED0, 1);
       vTaskDelay(250);
-
-      gpio_put(PICO_DEFAULT_LED_PIN, 0);
-
+      gpio_put(LED0, 0);
       vTaskDelay(250);
-
    }
+}
 
+void vBlinkTask1() {
+   for (;;) {
+      gpio_put(LED1, 1);
+      vTaskDelay(500);
+      gpio_put(LED1, 0);
+      vTaskDelay(500);
+   }
 }
 
 void main() {
+   gpio_init(LED0);
+   gpio_set_dir(LED0, GPIO_OUT);
 
-   gpio_init(PICO_DEFAULT_LED_PIN);
+   gpio_init(LED1);
+   gpio_set_dir(LED1, GPIO_OUT);
 
-   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-
-   xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+   xTaskCreate(vBlinkTask0, "Blink Task 0", 128, NULL, 1, NULL);
+   xTaskCreate(vBlinkTask1, "Blink Task 1", 128, NULL, 1, NULL);
 
    vTaskStartScheduler();
-
 }
 
 
