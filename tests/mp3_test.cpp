6d3b01a26eaf060a0963f54a91c8035148e12771
Alexandru Puscasu <palexandru2000@gmail.com>
2023-11-26 15:54:02 +0200

    remove log messages

--- tests/mp3_test.cpp
+++ tests/mp3_test.cpp
@@ -99,17 +99,17 @@ int main() {
             }
             while (true) {
                 count++;
-                printf("Skip: %d\n", skip);
+                // printf("Skip: %d\n", skip);
                 int next_sync = MP3FindSyncWord(decode_buf + skip, buf_size - skip);
                 if (next_sync == -1) {
-                    printf("Syncword not found\n");
+                    // printf("Syncword not found\n");
                     skip = BUF_MAX;
                 } else if (next_sync == 0) {
                     int bytesLeft = buf_size - skip;
-                    printf("Decode frame\n");
+                    // printf("Decode frame\n");
                     int ret = MP3Decode(decode_buf + skip, &bytesLeft, buf_s16, 0);
                     if (buf_size - skip == bytesLeft) {
-                        printf("Miss syncword\n");
+                        // printf("Miss syncword\n");
                         skip += 2; // miss sync bytes, skipt 2 bytes and search again
                     } else {
                         skip += buf_size - skip - bytesLeft;
@@ -117,7 +117,7 @@ int main() {
                         if (ret == 0) {
                             int play = 1152;
                             while (play > 0) {
-                                printf("Create buf %d\n", play);
+                                // printf("Create buf %d\n", play);
                                 audio_buffer_t *buffer = take_audio_buffer(ap, true);
                                 int16_t* samples = (int16_t*) buffer->buffer->bytes;
                                 int16_t* buf = buf_s16 + (1152 - play);
@@ -136,21 +136,21 @@ int main() {
                                     samples[i*2+1] = buf[i*2+1];
                                 }
 
-                                printf("Play buf\n");
+                                // printf("Play buf\n");
                                 give_audio_buffer(ap, buffer);
                             }
                         } else {
-                            printf("Decode error: %d\n", ret);
+                            // printf("Decode error: %d\n", ret);
                         }
                     }
                 } else {
-                    printf("Skip frame\n");
+                    // printf("Skip frame\n");
                     skip += next_sync;
                 }
             
                 //printf("samples: %d, frame: %d\n", samples, info.frame_bytes);
                 if (skip >= BUF_MAX) {
-                    printf("Reach buf limit\n");
+                    // printf("Reach buf limit\n");
                     memcpy(decode_buf, decode_buf + BUF_MAX, BUF_MAX);
                     skip -= BUF_MAX;
                     buf_size -= BUF_MAX;
